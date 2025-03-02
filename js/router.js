c17f8dcff00a306675ccd1bc01d084c037e7ec19
Manal Batta <manal.batta.1234@gmail.com>
2025-03-02 11:56:11 +0200

    Chat Page

--- js/router.js
+++ js/router.js
@@ -1,26 +1,39 @@
+let interval; //a global variable to store the interval for the home page real time clock
+
 const mainRoute = async (isLogged) => {
   const index = document.getElementById("app");
   try {
     const response = await fetch(isLogged ? "dashboard.html" : "signin.html");
     const html = await response.text();
     index.innerHTML = html;
-    dashboardRout("home"); //By default display the home page
+    dashboardRout("chat"); //By default display the home page
   } catch (error) {
     console.error("Error fetching the page:", error);
   }
 };
 
-const dashboardRout = async (page) => {
+const dashboardRout = async (page, event) => {
   const index = document.getElementById("content");
   try {
     const response = await fetch(`../pages/${page}.html`);
     const html = await response.text();
     index.innerHTML = html;
-    loadChart();
+    clearInterval(interval);
+    if (page === "home") interval = homeIntialize();
+    else if (page == "chat") loadContacts();
+    highlightSelectedPageLink(event.target);
   } catch (error) {
     console.error("Error fetching the page:", error);
   }
 };
 
+const homeIntialize = () => {
+  loadChart();
+  updateTime();
+  return setInterval(updateTime, 1000);
+};
 
 mainRoute(true);
+/* document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM is fully loaded and parsed.");
+}); */
