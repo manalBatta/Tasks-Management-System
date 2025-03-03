0a222613c5981fccf144dbd6538b398b561a8582
Fatima1Nasser <s12112649@stu.najah.edu>
2025-03-03 11:49:11 +0200

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,11 @@
+//Hello Fatima this is temporary so I can do my chat page part //Manal
+localStorage.setItem(
+  "user",
+  JSON.stringify({
+    id: 1,
+    username: "adminUser",
+    password: "hashedpassword1",
+    role: "admin",
+    universityID: null,
+  })
+);
